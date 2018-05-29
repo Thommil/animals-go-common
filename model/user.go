f347e1404d7471e5b56eaa99e3cde0fc279137ac
Thomas Millet <thommil@pc67.home>
2018-05-29 17:28:47 +0200

    Mongo added

--- model/user.go
+++ model/user.go
@@ -1,7 +1,11 @@
 package model
 
+import "github.com/globalsign/mgo/bson"
+
 // User model definition
 type User struct {
-	// Id of the user
-	ID string
+	// ID of the user
+	ID bson.ObjectId `json:"id" bson:"_id,omitempty"`
+	// Username of the user
+	Username string `json:"username" bson:"username"`
 }
