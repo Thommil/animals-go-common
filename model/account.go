e56340971a5deffd557e2a36edc6123b4bad634f
Thomas Millet <thommil@pc67.home>
2018-05-31 00:31:55 +0200

    Added google auth

--- model/account.go
+++ model/account.go
@@ -1,6 +1,11 @@
 package model
 
-import "github.com/globalsign/mgo/bson"
+import (
+	"fmt"
+
+	"github.com/globalsign/mgo"
+	"github.com/globalsign/mgo/bson"
+)
 
 // Account model definition
 type Account struct {
@@ -11,5 +16,54 @@ type Account struct {
 	// ExternalID of the account
 	ExternalID string `json:"external_id,omitempty" bson:"external_id,omitempty"`
 	// User ID linked to this account
-	UserID string `json:"user_id" bson:"user_id"`
+	UserID string `json:"user_id,omitempty" bson:"user_id,omitempty"`
+}
+
+// CreateOrUpdateAccount from the parameter and returns the created/updated account
+func CreateOrUpdateAccount(database *mgo.Database, account *Account) (*Account, error) {
+	var err error
+	if account.ID != "" {
+		//Update
+		err = database.C("account").UpdateId(account.ID, account)
+	} else {
+		//Create
+		account.ID = bson.NewObjectId()
+		err = database.C("account").Insert(account)
+	}
+	if err != nil {
+		return nil, err
+	}
+	return account, nil
+}
+
+// FindAccount returns a Mongo Query to parse result, the search query is Mongo based too
+func FindAccount(database *mgo.Database, query interface{}) *mgo.Query {
+	return database.C("account").Find(query)
+}
+
+// FindAccountByID allows to get a single account from its ID
+func FindAccountByID(database *mgo.Database, id string) (*Account, error) {
+	account := &Account{}
+	if bson.IsObjectIdHex(id) {
+		err := database.C("account").FindId(bson.ObjectIdHex(id)).One(account)
+		if err != nil {
+			return nil, err
+		}
+		return account, nil
+	}
+	return nil, fmt.Errorf("Invalid account ID")
+}
+
+// DeleteAccountByID allows to delete an existing account from its ID
+// Deletes also the linked user
+func DeleteAccountByID(database *mgo.Database, id string) error {
+	if bson.IsObjectIdHex(id) {
+		account, err := FindAccountByID(database, id)
+		if err != nil {
+			return err
+		}
+		DeleteUserByID(database, account.UserID)
+		return database.C("account").RemoveId(bson.ObjectIdHex(id))
+	}
+	return fmt.Errorf("Invalid account ID")
 }
--- model/user.go
+++ model/user.go
@@ -17,12 +17,24 @@ type User struct {
 
 // CreateOrUpdateUser from the parameter and returns the created/updated user
 func CreateOrUpdateUser(database *mgo.Database, user *User) (*User, error) {
-	return nil, fmt.Errorf("Not implemented")
+	var err error
+	if user.ID != "" {
+		//Update
+		err = database.C("user").UpdateId(user.ID, user)
+	} else {
+		//Create
+		user.ID = bson.NewObjectId()
+		err = database.C("user").Insert(user)
+	}
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
 }
 
 // FindUser returns a Mongo Query to parse result, the search query is Mongo based too
-func FindUser(database *mgo.Database, query interface{}) (*mgo.Query, error) {
-	return nil, fmt.Errorf("Not implemented")
+func FindUser(database *mgo.Database, query interface{}) *mgo.Query {
+	return database.C("user").Find(query)
 }
 
 // FindUserByID allows to get a single user from her ID
@@ -42,7 +54,6 @@ func FindUserByID(database *mgo.Database, id string) (*User, error) {
 func DeleteUserByID(database *mgo.Database, id string) error {
 	if bson.IsObjectIdHex(id) {
 		return database.C("user").RemoveId(bson.ObjectIdHex(id))
-	} else {
-		return fmt.Errorf("Invalid user ID")
 	}
+	return fmt.Errorf("Invalid user ID")
 }
